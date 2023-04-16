c0b9b098fb3da35bb4ec4196e8aa676d6bc1b9c3
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-04-16 22:42:33 +0300

    Changed to emotion dataset with 6 labels

--- train.py
+++ train.py
@@ -5,49 +5,55 @@ from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer
 import numpy as np
 from datasets import load_metric
+from sklearn.metrics import accuracy_score, f1_score
+import torch
 
-tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=2)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-imdb = load_dataset('imdb')
+model_name = 'bert-base-uncased'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = (AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=6).to(device))
 
-small_train_dataset = imdb['train'].shuffle(seed=42).select([i for i in range(3000)])
-small_test_dataset = imdb['test'].shuffle(seed=42).select([i for i in range(300)])
+emotions = load_dataset('emotion')
 
-def preprocess_function(examples):
-   return tokenizer(examples["text"], truncation=True)
+def tokenize(batch):
+   return tokenizer(batch["text"], padding=True, truncation=True)
  
-tokenized_train = small_train_dataset.map(preprocess_function, batched=True)
-tokenized_test = small_test_dataset.map(preprocess_function, batched=True)
+emotions_encoded = emotions.map(tokenize, batched=True, batch_size=None)
+emotions_encoded.set_format("torch", columns=["input_ids", "attention_mask", "label"])
 
-def compute_metrics(eval_pred):
-   load_accuracy = load_metric("accuracy")
-   load_f1 = load_metric("f1")
-  
-   logits, labels = eval_pred
-   predictions = np.argmax(logits, axis=-1)
-   accuracy = load_accuracy.compute(predictions=predictions, references=labels)["accuracy"]
-   f1 = load_f1.compute(predictions=predictions, references=labels)["f1"]
-   return {"accuracy": accuracy, "f1": f1}
+# Print the features of the encoded emotions
+print(emotions_encoded["train"].features)
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    f1 = f1_score(labels, preds, average="weighted")
+    acc = accuracy_score(labels, preds)
+    return {"accuracy": acc, "f1": f1}
+
+batch_size = 64
+logging_steps = len(emotions_encoded["train"]) // batch_size
 
 training_args = TrainingArguments(
     output_dir='./results',
     learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    num_train_epochs=2,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    load_best_model_at_end=True,
+    metric_for_best_model="f1",
     weight_decay=0.01,
+    evaluation_strategy="epoch",
+    num_train_epochs=8,
     save_strategy="epoch",
+    disable_tqdm=False,
 )
 
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_train,
-    eval_dataset=tokenized_test,
-    tokenizer=tokenizer,
-    data_collator=data_collator,
+    train_dataset=emotions_encoded["train"],
+    eval_dataset=emotions_encoded["validation"],
     compute_metrics=compute_metrics,
 )
 
